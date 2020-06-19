be6af355e041276d0c284063c4e6e060a0e4a3b8
Sebastian <monika.lukowska@gmail.com>
2020-06-19 18:39:21 +0200

    add missing end

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Roster
     
     config.generators do |g| 
       g.test_framework :rspec 
-      g.fixture_replacement :factory_bot, dir: 'spec/factories'   
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end   
   end
 end
